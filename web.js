5637f929b0a119a62b7d301df0dd4d7d9e44af70
Andrew Kos <akos@theonion.com>
2014-11-07 11:41:00 -0600

    Added node-open to open web browser once server starts

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var open = require('open');
 
 var app = connect();
 
-app.use(serveStatic('static', {'index': ['example.html']}));
+// some constants
+var PORT = 3000;
+var HOME_PAGE = 'example.html';
+
+// start server
+app.use(serveStatic('static', {'index': [HOME_PAGE]}));
 app.use(serveStatic('bower_components', {'index': false}));
 app.listen(3000);
+
+// open browser
+open('http://localhost:' + PORT + '/' + HOME_PAGE);
